9f7aed9b0276b2f0a40804b5e71790ac37f1b9ca
Hyhyhyhyhyhyh <44884619+Hyhyhyhyhyhyh@users.noreply.github.com>
2018-11-29 09:56:19 +0800

    Create create_table.sql

---
+++ create_table.sql
@@ -0,0 +1,9 @@
+CREATE TABLE dim_area 
+( 
+   province_id   VARCHAR2(30),  --省ID 
+   province_name VARCHAR2(30),  --省名 
+   city_id       VARCHAR2(30),  --市ID 
+   city_name     VARCHAR2(30),  --市名 
+   county_id     VARCHAR2(30),  --县ID 
+   county_name   VARCHAR2(30)   --县名 
+); 
